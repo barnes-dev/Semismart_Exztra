6aa18773af0da917e9c7d5ec019ac763411b27a2
NiccZee <106014661+NiccZee@users.noreply.github.com>
2026-04-17 23:26:26 +0200

    Refactor sensor initialization and reading logic

--- src/sensors/sensors.cpp
+++ src/sensors/sensors.cpp
@@ -14,88 +14,90 @@ static bool sensorInErrorState = false;
 bool sht4Available = false;
 
 void initSensors() {
-    if (!sht4Available) {
-        sht4Available = sht4.begin();
-    }
+  if (!sht4Available) {
+    sht4Available = sht4.begin();
+  }
 }
 
 void readSensors() {
-    if (!sht4Available) {
-        return;
-    }
-    sensors_event_t humidity_event, temp_event;
-    if (sht4.getEvent(&humidity_event, &temp_event)) {
-        extern SensorManager sensorManager;
-        int16_t tempRaw = (int16_t)(temp_event.temperature * 10.0f);
-        int16_t humRaw = (int16_t)(humidity_event.relative_humidity * 10.0f);
-        sensorManager.updateTemperature(temp_event.temperature, tempRaw);
-        sensorManager.updateHumidity(humidity_event.relative_humidity, humRaw);
-        sensorManager.setValid(tempRaw > 0 && humRaw > 0);
-    }
+  if (!sht4Available) {
+    return;
+  }
+  sensors_event_t humidity_event, temp_event;
+  if (sht4.getEvent(&humidity_event, &temp_event)) {
+    extern SensorManager sensorManager;
+    int16_t tempRaw = (int16_t)(temp_event.temperature * 10.0f);
+    int16_t humRaw = (int16_t)(humidity_event.relative_humidity * 10.0f);
+    sensorManager.updateTemperature(temp_event.temperature, tempRaw);
+    sensorManager.updateHumidity(humidity_event.relative_humidity, humRaw);
+    sensorManager.setValid(tempRaw > 0 && humRaw > 0);
+  }
 }
 
 bool validateTemperatureReading(int16_t temp) {
-    return temp != (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f) && 
-           !isnan(temp) && 
-           temp >= SENSOR_TEMP_MIN && 
-           temp <= SENSOR_TEMP_MAX && 
-           (lastValidTemperature <= 0 || (temp - lastValidTemperature) <= (int16_t)(SECURITY_SENSOR_MAX_TEMP_CHANGE * 10.0f));
+  return temp != (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f) && !isnan(temp) && temp >= SENSOR_TEMP_MIN && temp <= SENSOR_TEMP_MAX && (lastValidTemperature <= 0 || (temp - lastValidTemperature) <= (int16_t)(SECURITY_SENSOR_MAX_TEMP_CHANGE * 10.0f));
 }
 
 int16_t readThermistorTemperature() {
 
 #if TEMP_SENSOR
-#if ARDUINO_ESP32
-    int analogValue = analogRead(NTC_PIN) / 4;
+  // Many ESP32 cores return 12-bit ADC; normalize to 10-bit (0-1023)
+  float analogValue = analogRead(NTC_PIN);
 #else
-    int analogValue = analogRead(NTC_PIN);
+  float analogValue = SECURITY_NO_SENSOR;
 #endif
-#else
-    int analogValue = SECURITY_NO_SENSOR;
-#endif
-    uint32_t currentTime = millis();
-    if (analogValue < SECURITY_MIN_ADC_VALUE || analogValue > SECURITY_MAX_ADC_VALUE) {
-        if (!sensorInErrorState) lastErrorTime = currentTime, sensorInErrorState = true;
-        return (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f);
-    }
-    // Optimize thermistor calculation with pre-calculated constants
-    static const float ADC_RATIO = ADC_MAX / THERMISTOR_SERIES_RESISTOR;
-    static const float STEINHART_A = 0.001129148;
-    static const float STEINHART_B = 0.000234125;
-    static const float STEINHART_C = 0.0000000876741;
-    
-    float R_NTC = THERMISTOR_SERIES_RESISTOR * (ADC_RATIO / (float)analogValue - 1.0);
-    float steinhart = (R_NTC > 0) ? log(R_NTC) : 0;
-    steinhart = 1.0 / (STEINHART_A + (STEINHART_B * steinhart) + (STEINHART_C * steinhart * steinhart * steinhart));
-    int16_t temp_c = (int16_t)((steinhart - 273.15) * 10.0f);
-    if (firstReading) {
-        lastValidTemperature = temp_c;
-        firstReading = false;
-        lastValidReadTime = currentTime;
+
+  uint32_t currentTime = millis();
+  if (analogValue < SECURITY_MIN_ADC_VALUE || analogValue > SECURITY_MAX_ADC_VALUE) {
+    if (!sensorInErrorState) lastErrorTime = currentTime, sensorInErrorState = true;
+    return (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f);
+  }
+
+  // Analog -> resistance conversion for voltage divider:
+  // Vout = analogValue/bit-resolution * Vcc
+  // R_ntc = R_series * (Vcc / Vout - 1)
+  float vout = (analogValue / ADC_MAX) * VCC;
+  // Protect division by zero / out-of-range readings
+  if (vout <= 0.0f || vout >= VCC) {
+    if (!sensorInErrorState) lastErrorTime = currentTime, sensorInErrorState = true;
+    return (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f);
+  }
+
+  float R_NTC = THERMISTOR_SERIES_RESISTOR * (VCC / vout - 1.0f);
+
+  // Beta equation: 1/T = 1/T0 + (1/B) * ln(R/R0)
+  const float T0_K = 25.0f + 273.15f;
+  float invT = (1.0f / T0_K) + (1.0f / THERMISTOR_BETA) * log(R_NTC / THERMISTOR_NOMINAL_RESISTANCE);
+  float tempK = 1.0f / invT;
+  int16_t temp_c = (int16_t)((tempK - 273.15f) * 10.0f);
+
+  if (firstReading) {
+    lastValidTemperature = temp_c;
+    firstReading = false;
+    lastValidReadTime = currentTime;
+    sensorInErrorState = false;
+    return temp_c;
+  }
+  if (sensorInErrorState) {
+    if (currentTime - lastErrorTime > SECURITY_SENSOR_RESET_TIME) {
+      if (validateTemperatureReading(temp_c)) {
         sensorInErrorState = false;
+        lastValidTemperature = temp_c;
+        lastValidReadTime = currentTime;
         return temp_c;
-    }
-    if (sensorInErrorState) {
-        if (currentTime - lastErrorTime > SECURITY_SENSOR_RESET_TIME) {
-            if (validateTemperatureReading(temp_c)) {
-                sensorInErrorState = false;
-                lastValidTemperature = temp_c;
-                lastValidReadTime = currentTime;
-                return temp_c;
-            } else {
-                lastErrorTime = currentTime;
-                return (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f);
-            }
-        }
-        return (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f);
-    }
-    if (!validateTemperatureReading(temp_c)) {
-        sensorInErrorState = true;
+      } else {
         lastErrorTime = currentTime;
         return (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f);
+      }
     }
-    lastValidTemperature = temp_c;
-    lastValidReadTime = currentTime;
-    return temp_c;
+    return (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f);
+  }
+  if (!validateTemperatureReading(temp_c)) {
+    sensorInErrorState = true;
+    lastErrorTime = currentTime;
+    return (int16_t)(SECURITY_SENSOR_ERROR_VALUE * 10.0f);
+  }
+  lastValidTemperature = temp_c;
+  lastValidReadTime = currentTime;
+  return temp_c;
 }
-
